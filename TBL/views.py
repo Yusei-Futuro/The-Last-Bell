a7df431744b90b2aca3a9fa78438803470f7db1b
alray-sudo <alessrayl@gmail.com>
2025-09-27 12:17:02 -0300

    Empezando la parte de los model y templa

--- TBL/views.py
+++ TBL/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
+
+def hello(request):
+    return HttpResponse("Hola mundo")
