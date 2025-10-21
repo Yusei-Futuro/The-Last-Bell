85292ba90fca613148f256df116a5358cd3ffb7f
alray-sudo <alessrayl@gmail.com>
2025-10-20 21:38:33 -0300

    Me quedan creo que unos 4 modelos mas y maanana para continuar con los del gamesave en model con Simon

--- TBL/models.py
+++ TBL/models.py
@@ -1,5 +1,6 @@
 from random import choice
 from tkinter.constants import CASCADE
+from tkinter.font import names
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -105,12 +106,122 @@ class Location(models.Model):
     def __str__(self):
         return f"{self.locations} (Día {self.day_location})"
 
-
 class Character(models.Model):
     name = models.CharField(max_length=100)
     backstory = models.TextField()
-    trait = models.CharField(max_length=100)  # Ej: "Autista", "Migrante", "No binarie"
-    image_url = models.URLField(blank=True)
+    trait = models.CharField(max_length=100)
+    image_url = models.URLField(
+        blank=True,
+        null=True,
+                                )
+    interesting=models.TextField(
+        help_text="Intereses del personaje para abordar mas la historia"
+    )
+
+    personality=models.TextField(
+        help_text="Personalidad"
+    )
+    is_friend=models.IntegerField(
+        default=20,
+        help_text="Puntos necesarios para considerarse amigo"
+    )
+
+    class Meta:
+        verbose="Personaje"
+        verbose_plural="Personajes"
+        ordering=["name"]
+
+    def __str__(self):
+        return self.name
+
+class Prologo(models.Model):
+
+    title=models.CharField(
+        max_length=200,
+        default="El comienzo de una nueva vida",
+        help_text="Para el titulo del juego"
+    )
+
+    orden=models.IntegerField(
+        unique=True,
+        help_text="Aca quiero lo del orden de aparacion de la cinematicas"
+    )
+
+    text_prologo=models.TextField(
+        help_text="Texto que aparecera en la pantalla"
+    )
+
+    content_dialogue=models.CharField(
+        max_length=200,  #Faltan aca escribir los dialogos ok lo dejare expresado
+        choices=[
+            ('narration', 'Narración'),
+            ('dialogue', 'Diálogo (Mamá, Álex, etc.)'),
+            ('came_thought', 'Pensamiento de Came'),
+        ]
+    )
+
+    character = models.ForeignKey(
+        'Character',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        help_text="Personaje que habla (si es diálogo)"
+    )
+
+    class Meta:
+        verbose_name = "Prólogo"
+        verbose_name_plural = "Prólogos"
+        ordering = ['orden']
+
+    def __str__(self):
+        return f"Prólogo {self.orden}: {self.content_dialogue()}"
+
+class Situations(models.Model):
+    title=models.CharField(
+        max_length=20,
+        help_text="Nombre de la situacion"
+    )
+
+    day=models.IntegerField(
+        unique=True,
+        help_text="Numero de situacion en la que me encuentro"
+    )
+
+    locations=models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        related_name="situations"
+    )
+
+    character=models.ForeignKey(
+        Character,
+        on_delete=models.CASCADE,
+        related_name="situations",
+        blank=True,
+        null=True
+    )
+
+    contexto_situation=models.TextField(
+        help_text="Para hacer el contexto de la situacion en la que se encuentra"
+    )
+
+    is_gameplay=models.BooleanField(
+        default=True,
+        help_text="Campo para saber si es jugable"
+    )
+
+    orden_dialogue=models.IntegerField(
+        default=0,
+        help_text="Lineas de dialogo del day"
+    )
+
+    class Meta:
+        verbose_name = "Situación"
+        verbose_name_plural = "Situaciones"
+        ordering = ['day']
+
+    def __str__(self):
+        return f"Día {self.day}: {self.title}"
 
 class Dialogue(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
@@ -122,3 +233,11 @@ class Choice(models.Model):
     text = models.CharField(max_length=200)
     consequence = models.TextField()
     next_dialogue = models.ForeignKey(Dialogue, on_delete=models.SET_NULL, null=True, blank=True,related_name="choice_to")
+
+class GameSave(models.Model): #Para hablar con simon manana sobre esto
+
+    perfil=models.ForeignKey(
+        Username,
+        on_delete=models.CASCADE,
+        related_name="gamesave"
+    )
