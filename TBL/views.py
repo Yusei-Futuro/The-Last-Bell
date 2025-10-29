ee6ebbb281583772de74b7e779f0e182b276d6c7
alray-sudo <alessrayl@gmail.com>
2025-10-29 19:12:58 -0300

    Termine el parte de las vistas falta la parte del gamesave

--- TBL/views.py
+++ TBL/views.py
@@ -6,12 +6,51 @@ from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from .models import Username, Situations
+#Falta obligatoriamente el GameSave() importarlo aca
 
 # Create your views here.
 
 @login_required()
 def main_game(request):
-    return render(request,"main game.html")
+
+    #Esta parte del codigo esta establecida para el desarrollo del menu del game con las opciones
+    #Continuar partida si existe, cargar partida, resultados,salir
+
+    user=request.user.Username
+    #Buscame la parte de guardar partida
+
+    context = {
+        'player_profile': user,
+        #Tengo aca que buscar el ultimo guardado
+    }
+
+    return render(request,"main game.html", context)
+
+@login_required
+def new_game(request):
+
+    #Funcion que reinicia los ajustes a predeterminados
+    user=request.user.Username
+
+    user.day = 1
+    user.situation_complet = False
+    user.friend = 0
+    user.friends_count = 0
+    user.game_completed = False
+    user.save()
+
+    #Aca hay que poner GameSave pero la parte del juego seria eliminada el progreso anterior
+
+    return redirect("prologue")
+
+@login_required
+def prologue(request): #Aca esta la parte del prologo del juego
+    return HttpResponse("Hello word")
+
+@login_required
+def load_game(request):#Aca va el load game del juego
+    return HttpResponse("Hello word")
 
 def main(request):
     return render(request, "main_base.html")
@@ -61,8 +100,6 @@ def singin(request):
         else:
             login(request, user)
             return redirect("/main/")
-
-#Parte de pablo sobre el game
 
 def dialogue_view(request, dialogue_id):
     dialogue = get_object_or_404(Dialogue, pk=dialogue_id)
